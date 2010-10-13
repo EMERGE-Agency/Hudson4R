1a7964a2013441e9249d02fd7d2fb7b1ce7ef17f
Justin B <justin.d.barry@gmail.com>
2010-10-13 11:15:10 -0700

    added get_projects method. JSON call is wrapped in private method.

--- lib/hudson_tool.rb
+++ lib/hudson_tool.rb
@@ -1,3 +1,42 @@
-module	Hudson4R
+require 'net/http'
+require 'uri'
+require 'json'
 
+=begin
+  This modules purpose is two-fold. 
+  It is meant to pull information 
+  from Hudson and make it availible. 
+  In addition, it should trigger 
+  build using passed in parameters.
+=end
+
+module	Hudson4R
+  class HudsonTool
+    def initialize(server)
+      @hudson = server += '/api/json'
+    end
+    
+    #Get the availible jobs and returns them in a hash. The job names are the key.
+    def get_jobs
+      jobs = {}
+      get_api['jobs'].each do |job|
+        jobs.store(job['name'],job)
+      end
+      jobs
+    end
+    
+    def trigger_build
+      
+    end
+    
+    private
+    def get_api
+      url = URI.parse(@hudson)
+      req = Net::HTTP::Get.new(url.path)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      JSON::Pure::Parser.new(res.body).parse
+    end
+  end
 end
