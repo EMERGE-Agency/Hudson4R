dce12bd28972eb46674ac52f89f978df25d17448
Justin B <justin.d.barry@gmail.com>
2010-10-14 09:44:45 -0700

    Moved to justin_sandbox_com as a test example. This has parameters, an interesting test case.

--- test/tc_hudson_tool.rb
+++ test/tc_hudson_tool.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 gem 'test-unit'
 require 'test/unit'
 $LOAD_PATH << './lib'
-require 'lib/hudson_tool'
+require 'lib/hudson'
 
 class TC_Hudson4R_HudsonTool < Test::Unit::TestCase
   def setup
@@ -16,14 +16,22 @@ class TC_Hudson4R_HudsonTool < Test::Unit::TestCase
     assert(result['justin_sandbox_com'])
   end
   
-  def test_trigger_build
-    result = @hudson.trigger_build('koopmanostbo_com')
-    assert(result)
+  def test_get_queue
+    result = @hudson.get_queue
+    assert(result.has_key? 'items')
+  end
+  
+  def test_trigger_build_good_job
+    assert(@hudson.trigger_build('justin_sandbox_com', {'issue_key'=>'SP-4'}))
+  end
+  
+  def test_trigger_build_bad_job
+    assert_false(@hudson.trigger_build('bad_company_com'))
   end
   
   def test_find_job
-    result = @hudson.find_job('koopmanostbo_com')
-    assert_equal(result['name'], 'koopmanostbo_com')
+    result = @hudson.find_job('justin_sandbox_com')
+    assert_equal(result['name'], 'justin_sandbox_com')
   end
   
   def teardown
